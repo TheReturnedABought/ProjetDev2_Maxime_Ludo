4b3a195a390227400afd5ac34439534a49e2b264
BarryVanAllen <theo6740@hotmail.fr>
2024-11-15 13:58:36 +0100

    Update Item.py

--- classes/Item.py
+++ classes/Item.py
@@ -6,9 +6,21 @@ class Item:
         self.slot = slot #la place qu'il prend dans l'inventaire
 
     def equip(self):
+        """
+        PRE: Lobjet doit être dans l'inventaire et ne doit pas être déjà équipé.
+        POST: L'objet est marqué comme équipé.
+        Raise: Erreur si l'objet est déjà équipé.
+
+        """
         pass
 
     def unequip(self):
+        """
+
+        PRE: L'objet doit être équipé.
+        POST:L'objet n'est plus marqué comme équipé.
+        RAISE: RunTimeError si l'objet n'est pas équipé.
+        """
         pass
 
     def examine(self):
