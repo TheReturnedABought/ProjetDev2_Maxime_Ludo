f1c8b558cb48c1b492f99aa09db5171dd33d409b
BarryVanAllen <theo6740@hotmail.fr>
2024-11-15 11:08:04 +0100

    Update Player.py
    mis specs

--- classes/Player.py
+++ classes/Player.py
@@ -6,11 +6,29 @@ class Player(Character):
         self.character_budget = 10 #argent du perso
         super().__init__(name, health, attack_power)
 
+
     def add_to_inventory(self, item):
         self.inventory.append(item)
 
+        """
+        Ajoute un objet désigné à l'inventaire du personnage.
+        
+        PRE : item est une string représentant un objet physique.
+        Post : l'item doit être dans l'inventaire après l'ajout.
+
+        """
+
+
     def remove_from_inventory(self, item):
         self.inventory.remove(item)
+
+        """
+        Enlève un item désigné à l'inventaire du personnage.
+        
+        PRE : item doit être dans l'inventaire, inventaire.
+        POST: item doit avoir disparu de l'inventaire.
+        """
+
 
     def get_inventory(self):
         if len(self.inventory) == 0:
@@ -19,6 +37,14 @@ class Player(Character):
             print(f"| Objet(s) dans votre inventaire :")
             for objet in self.inventory:
                 print(f"| - {objet}")
+
+        """
+        Montre l'inventaire à l'écran.
+        
+        PRE : l'inventaire doit être initialiser et capable de recevoir des items.
+        POST: retourne l'inventaire sous forme de liste avec puce.
+        """
+
 
     def save(self):
         pass #fonction pour sauvegarder la partie a completer
