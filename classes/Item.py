82ce1543550a8bd031c169dffac04e3d1ddc584f
BarryVanAllen <theo6740@hotmail.fr>
2024-12-03 14:23:46 +0100

    Update Item.py

--- classes/Item.py
+++ classes/Item.py
@@ -7,10 +7,11 @@ class Item:
         self.equiped = equiped
     def equip(self):
         """
-       "équipe" l'item, le marque comme "dans la main" du joueur.
+        "équipe" l'item, le marque comme "dans la main" du joueur.
 
-        PRE: L'objet que le joueur veux équiper.
-        POST: L'objet est marqué comme équipé.
+        PRE:
+        POST:Si "equiped = False" on le change en vrai. 
+            Si il a un slot (le paramètre slot a une valeur tel que main, tête) : L'objet est marqué comme équipé.
         Raise: renvoie une erreur si l'objet est déjà équipé ou si il n'y a aucun objet dans l'inventaire.
 
         """
@@ -19,9 +20,10 @@ class Item:
     def unequip(self):
         """
         "Déséquipe" l'item de la main du joueur.
-        PRE: 
-        POST:L'objet n'est plus marqué comme équipé.
-        RAISE: RunTimeError si l'objet n'est pas équipé.
+    
+    PRE: 
+    POST:L'objet n'est plus marqué comme équipé et le paramètre slot prend la valeur None.
+    RAISE: RunTimeError si l'objet n'est pas équipé.
 
         """
         pass
